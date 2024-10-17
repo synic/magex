03d73e3718ad658a00125f999cd6f0dca8b5e659
Adam Olsen <arolsen@gmail.com>
2024-10-16 19:31:45 -0600

    feat: install to location

--- magex.go
+++ magex.go
@@ -1,88 +1,108 @@
 package magex
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 
 	"github.com/magefile/mage/sh"
 	"golang.org/x/mod/modfile"
 )
 
-// ModuleVersionFromModFile pulls the version of modulePath from the go.mod file
-// specified by modFilePath. This is useful in combination with MaybeInstallTool when
-// the tool is paired with a module that is used in the code, for example,
-// templ (https://templ.guide/) where you import the module, but it also has a cli
-// codegen command and they need to be the same version. This allows your go.mod file
-// to be the source of truth.
-func ModuleVersionFromModFile(modFilePath, modulePath string) (string, error) {
-	data, err := os.ReadFile(modFilePath)
+// ModuleVersion pulls the version of `name` from "./go.mod" This is useful
+// in combination with MaybeInstallTool when the tool is paired with a module
+// that is used in the code, for example, templ (https://templ.guide/) where
+// you import the module, but it also has a cli codegen command and they need
+// to be the same version. This allows your go.mod file to be the source of
+// truth.
+func ModuleVersion(name string) (string, error) {
+	data, err := os.ReadFile("go.mod")
 
 	if err != nil {
 		return "", nil
 	}
 
-	f, err := modfile.ParseLax(modFilePath, data, nil)
+	f, err := modfile.ParseLax("go.mod", data, nil)
 
 	if err != nil {
 		return "", err
 	}
 
 	for _, info := range f.Require {
-		if info.Mod.Path == modulePath {
+		if info.Mod.Path == name {
 			return info.Mod.Version, nil
 		}
 	}
 
-	return "", fmt.Errorf("error parsing go.mod, module path \"%s\" not found", modulePath)
-}
-
-// ModuleVersion pulls the version of modulePath from "./go.mod" This is useful
-// in combination with MaybeInstallTool when the tool is paired with a module
-// that is used in the code, for example, templ (https://templ.guide/) where
-// you import the module, but it also has a cli codegen command and they need
-// to be the same version. This allows your go.mod file to be the source of
-// truth.
-func ModuleVersion(path string) (string, error) {
-	return ModuleVersionFromModFile("go.mod", path)
+	return "", fmt.Errorf("error parsing go.mod, module path \"%s\" not found", name)
 }
 
 // MaybeInstallTool checks to see if a go command line tool is installed, and if not,
 // it installs it.
-func MaybeInstallTool(name, path, version string) (string, error) {
-	return maybeInstallTool(name, path, version, false)
-}
-
-// MaybeInstallToolV checks to see if a go command line tool is installed, and if not,
-// it installs it. Any output is sent to stdout, similar to sh.RunV
-func MaybeInstallToolV(name, path, version string) (string, error) {
-	return maybeInstallTool(name, path, version, true)
-}
-
-func maybeInstallTool(name, path, version string, runV bool) (string, error) {
-	var f func(cmd string, args ...string) error = sh.Run
-
-	if runV {
-		f = sh.RunV
-	}
-
-	binaryPath, err := exec.LookPath(name)
+func MaybeInstallTool(cmdName, modulePath, version string) (string, error) {
+	binaryPath, err := exec.LookPath(cmdName)
 
 	if err == nil {
 		return binaryPath, nil
 	}
 
-	err = f("go", "install", fmt.Sprintf("%s@%s", path, version))
+	fmt.Printf("Installing %s@%s...", cmdName, version)
+	err = sh.Run("go", "install", fmt.Sprintf("%s@%s", modulePath, version))
+	fmt.Println("done")
 
 	if err != nil {
 		return "", err
 	}
 
-	binaryPath, err = exec.LookPath(name)
+	binaryPath, err = exec.LookPath(cmdName)
 
 	if err != nil {
 		return "", err
 	}
 
 	return binaryPath, nil
+}
+
+// MaybeInstallToolToDest checks to see if the tool command exists at the destination,
+// and if not, attempts to install it.
+func MaybeInstallToolToDestination(
+	cmdName, modulePath, version, destination string,
+) (string, error) {
+	destination, err := filepath.Abs(destination)
+	cmd := path.Join(destination, cmdName)
+
+	if err != nil {
+		return "", fmt.Errorf("error converting %s to absolute path: %w", destination, err)
+	}
+
+	_, err = os.Stat(cmd)
+
+	if err == nil {
+		return cmd, nil
+	} else if !errors.Is(err, os.ErrNotExist) {
+		return "", err
+	}
+
+	fmt.Printf("Installing %s@%s to %s\n", cmdName, version, destination)
+	err = sh.RunWith(
+		map[string]string{"GOBIN": destination},
+		"go",
+		"install",
+		fmt.Sprintf("%s@%s", modulePath, version),
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	_, err = os.Stat(path.Join(destination, cmdName))
+
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return "", err
+	}
+
+	return cmd, nil
 }
